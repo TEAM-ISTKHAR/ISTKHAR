980e07271c14ec41b8d6f8730a70de825fd5cb88
𝗜𝗦𝗧𝗞𝗛𝗔𝗥 𝗕𝗔𝗕𝗬 <mdistkhar601@gmail.com>
2025-04-30 14:18:32 +0530

    Update play.py

--- ISTKHARX/utils/inline/play.py
+++ ISTKHARX/utils/inline/play.py
@@ -1,8 +1,7 @@
 import math
-from config import SUPPORT_CHAT, OWNER_USERNAME
+
 from pyrogram.types import InlineKeyboardButton
-from ISTKHARX import app
-import config
+
 from ISTKHARX.utils.formatters import time_to_seconds
 
 
@@ -34,49 +33,59 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "♡╌╌╌╌╌╌╌╌"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "╌♡╌╌╌╌╌╌╌"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "╌╌♡╌╌╌╌╌╌"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "╌╌╌♡╌╌╌╌╌"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "╌╌╌╌♡╌╌╌╌"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
+        bar = "╌╌╌╌╌♡╌╌╌"
     elif 60 <= umm < 70:
-        bar = "——————◉———"
+        bar = "╌╌╌╌╌╌♡╌╌"
     elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "╌╌╌╌╌╌╌♡╌"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "╌╌╌╌╌╌╌╌♡╌"
     else:
-        bar = "—————————◉"
+        bar = "╌╌╌╌╌╌╌╌╌♡"
     buttons = [
-                [
+        [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
         ],
+                          [
+            InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+        ],
                 [
-         InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)
+            InlineKeyboardButton(
+                text="[🇮🇳] 𝐎ᴡɴᴇʀ •", user_id=config.OWNER_ID,
+            ),
+            InlineKeyboardButton(
+                text="• 𝐔ᴘᴅᴀᴛᴇs •", url=config.SUPPORT_CHANNEL
+            ),
         ],
-        [
-         InlineKeyboardButton(text="• ᴏᴡɴᴇʀ •", url="https://t.me/ll_ISTKHAR_BABY_lll"),
-         InlineKeyboardButton(text="• ɢʀᴏᴜᴘ •", url="https://t.me/+PaEtaAu9DI9mYzc9"),
-        ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
 
 def stream_markup(_, chat_id):
     buttons = [
-        [
-         InlineKeyboardButton(text="• ᴏᴡɴᴇʀ •", url="https://t.me/ll_ISTKHAR_BABY_lll"),
-         InlineKeyboardButton(text="• ɢʀᴏᴜᴘ •", url="https://t.me/+PaEtaAu9DI9mYzc9"),
+                [
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -88,11 +97,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"SonaPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"ISTKHARPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"SonaPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"ISTKHARPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
@@ -103,7 +112,6 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         ],
     ]
     return buttons
-
 
 
 def livestream_markup(_, videoid, user_id, mode, channel, fplay):
